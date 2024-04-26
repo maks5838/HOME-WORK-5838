defc181735fe5eba376250a98f93adb83adc9ae5
maks5838 <maksvahrushev2000@gmail.com>
2024-04-26 18:58:42 +0300

    added new super program

--- main.py
+++ main.py
@@ -4,22 +4,3 @@ account1.deposit(500)
 account1.withdraw(200)
 account1.withdraw(1500)
 
-2завдання
-car1 = Car("Toyota", "Camry", 2020)
-print(car1.get_info())
-
-
-3 завдання
-employee1 = Employee("Іван", "Менеджер", 50000)
-print(employee1.get_salary_info())
-
-
-4 завдання
-print("Площа прямокутника:", rectangle1.calculate_area())
-print("Периметр прямокутника:", rectangle1.calculate_perimeter())
-
-
-
-5 завдання
-product1 = Product("Книга", 100, 3)
-product1.display_info()
