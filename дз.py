fc3f356a0c7c4b04994939e006b0132acde5f7d1
maks5838 <maksvahrushev2000@gmail.com>
2024-05-19 11:35:54 +0300

    added new super program

--- дз.py
+++ дз.py
@@ -1,47 +1,33 @@
-class Product:
-    def __init__(self, name, price, availability):
-        self.name = name
-        self.price = price
-        self.availability = availability
+import requests
+from bs4 import BeautifulSoup
 
-    def __str__(self):
-        return f"{self.name} - Price: ${self.price}, Availability: {self.availability}"
+base_url = 'http://books.toscrape.com/'
 
+def get_book_titles(url):
+    response = requests.get(url)
+    response.raise_for_status()
 
-class Cart:
-    def __init__(self):
-        self.items = []
+    soup = BeautifulSoup(response.text, 'html.parser')
+    books = soup.select('article.product_pod h3 a')
+    titles = [book['title'] for book in books]
 
-    def add_to_cart(self, product, quantity=1):
-        if product.availability >= quantity:
-            self.items.append((product, quantity))
-            product.availability -= quantity
-            print(f"{quantity} {product.name}(s) added to cart.")
+    return titles
+
+def get_all_book_titles():
+    titles = []
+    url = base_url
+    while True:
+        titles.extend(get_book_titles(url))
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        next_button = soup.select_one('li.next a')
+        if next_button:
+            next_page_url = next_button['href']
+            url = base_url + next_page_url
         else:
-            print("Insufficient stock!")
+            break
 
-    def remove_from_cart(self, product, quantity=1):
-        for item in self.items:
-            if item[0] == product:
-                if item[1] > quantity:
-                    item[1] -= quantity
-                    product.availability += quantity
-                    print(f"{quantity} {product.name}(s) removed from cart.")
-                else:
-                    self.items.remove(item)
-                    product.availability += item[1]
-                    print(f"All {product.name}(s) removed from cart.")
-                return
-        print(f"{product.name} is not in the cart.")
-
-    def calculate_total(self):
-        total = 0
-        for item in self.items:
-            total += item[0].price * item[1]
-        return total
-
-    def checkout(self):
-        total = self.calculate_total()
-        print(f"Total amount to pay: ${total}")
-        self.items = []
-        print("Thank you for shopping with us!")
+    return titles
+all_titles = get_all_book_titles()
+for title in all_titles:
+    print(title)
